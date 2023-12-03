6e383eda5a2fbbe354d6d0024f1e603c3dddbbc0
Alchemist of the web <nikitakzntcv@gmail.com>
2023-12-04 01:21:29 +0400

    update docker configs; add dotenv; add code for handling a simple hello-bot using aiogram

--- src/main.py
+++ src/main.py
@@ -1,11 +1,30 @@
-import os
+import asyncio
+from dotenv import dotenv_values
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
-# Get the environment variable
-token = os.getenv('TELEGRAM_TOKEN')
 
-# Check if the token is available
-if token:
-    print("Token:", token)
-else:
-    print("TELEGRAM_TOKEN not found")
+config = dotenv_values('.env')
 
+API_TOKEN = config.get('TOKEN')
+print(API_TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def send_welcome(message: types.Message):
+    await message.answer("Привет! Я бот, который отвечает на приветствия!")
+
+@dp.message(lambda message: message.text.lower() == 'привет')
+async def say_hi(message: types.Message):
+    await message.answer("Привет! Как дела?")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
