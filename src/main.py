385fa27b6070239248aa45d16ff82e038d7551a4
funkvay-star <pasha1928374655@gmail.com>
2023-12-09 18:43:59 +0400

    Add ocntent classification feature
    - Implement ContentHandler to classify messages as links, documents, or videos.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,47 @@
-import os
+import re
+import asyncio
+import logging
+import sys
+from os import getenv
+from typing import Any
 
-# Get the environment variable
-token = os.getenv('TELEGRAM_TOKEN')
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Document, Video
 
-# Check if the token is available
-if token:
-    print("Token:", token)
-else:
-    print("TELEGRAM_TOKEN not found")
+TOKEN = getenv('TELEGRAM_TOKEN')
 
+class TelegramBot:
+    def __init__(self, token: str):
+        self.bot = Bot(token)
+        self.dp = Dispatcher()
+
+        # Register the content handler
+        self.dp.message.register(self.content_handler)
+
+    async def content_handler(self, message: types.Message) -> None:
+        try:
+            message_content = message.text or ""
+            if re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message_content):
+                await message.answer("This is a link")
+            elif isinstance(message.document, Document):
+                await message.answer("This is a document")
+            elif isinstance(message.video, Video):
+                await message.answer("This is a video")
+            else:
+                await message.answer("Unknown content type")
+
+        except Exception as e:
+            logging.error(f"Error occurred: {e}")
+            await message.answer("An error occurred while processing the message.")
+
+    async def start_polling(self) -> None:
+        await self.dp.start_polling(self.bot)
+
+async def main() -> None:
+    # Initialize and start the Telegram Bot
+    bot = TelegramBot(TOKEN)
+    await bot.start_polling()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
